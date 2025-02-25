f7fa4d61941bdb671567cc9cbd6d3566734fbdf5
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-25 15:47:55 -0300

    feat: adiciona pacote repositories com classe UserRepository

---
+++ src/main/java/br/com/zup/security/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.zup.security.repositories;
+
+import br.com.zup.security.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUserName(String username);
+}
