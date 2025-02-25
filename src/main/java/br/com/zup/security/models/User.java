8ef97a24986dabf3afe4e10ca51e93283a672b41
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-25 08:17:13 -0300

    feat: Criado classe User no pacote model

--- src/main/java/br/com/zup/security/models/User.java
+++ src/main/java/br/com/zup/security/models/User.java
@@ -1,4 +1,33 @@
 package br.com.zup.security.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
 public class User {
+    @Id
+    private String userName;
+    private String password;
+
+    public User(){
+
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
