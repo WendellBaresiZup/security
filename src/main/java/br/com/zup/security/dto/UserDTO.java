a125073d61d88d5ba9860fe11c0bb1207933df51
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-26 10:41:31 -0300

    feat: Criado classe UserDTO com atributos e metodos

--- src/main/java/br/com/zup/security/dto/UserDTO.java
+++ src/main/java/br/com/zup/security/dto/UserDTO.java
@@ -1,4 +1,51 @@
 package br.com.zup.security.dto;
 
 public class UserDTO {
+    private String uuid;
+    private String userName;
+    private String password;
+    private Role role;
+
+    public UserDTO(){
+
+    }
+
+    public UserDTO(String uuid, String userName, String password, Role role) {
+        this.uuid = uuid;
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
---
+++ src/main/java/br/com/zup/security/repositories/AdminRepository.java
@@ -0,0 +1,4 @@
+package br.com.zup.security.repositories;
+
+public class AdminRepository {
+}
