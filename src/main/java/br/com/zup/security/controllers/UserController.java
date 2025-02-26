766afbbfb50d7040c43d0e47bb8b57d87d3f625b
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-26 11:28:33 -0300

    refactor: Criado metodo login para o realizar o endpoint post

--- src/main/java/br/com/zup/security/controllers/UserController.java
+++ src/main/java/br/com/zup/security/controllers/UserController.java
@@ -1,24 +1,36 @@
 package br.com.zup.security.controllers;
 
+import br.com.zup.security.dto.Role;
+import br.com.zup.security.dto.UserDTO;
+import br.com.zup.security.infra.JwtUtil;
+import br.com.zup.security.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JwtUtil jwtUtil;
 
-    @GetMapping("/user")
-    public String getUserInfo(){
-        return "User infromation. Department: " +
-                SecurityContextHolder.getContext().getAuthentication().getDetails();
-    }
 
-    @Secured("ROLE_ADMIN")
-    @GetMapping("/admin")
-    public String getAdminInfo() {
-        return "Admin Information";
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserDTO userDTO) {
+        Optional<User> userOptional = userService.findByUserName(userDTO.getUserName());
+
+        if (userOptional.isEmpty() || !userOptional.get().getPassword().equals(userDTO.getPassword())) {
+            return ResponseEntity.status(401).body("Credenciais inválidas");
+        }
+        User user = userOptional.get();
+        String token = jwtUtil.createToken(user.getUserName(), "IT", "ROLE_USER");
+
+        return ResponseEntity.ok().body("Bearer " + token);
     }
 }
