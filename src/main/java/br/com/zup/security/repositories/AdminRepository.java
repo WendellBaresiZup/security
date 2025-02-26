a5f214e24044bcc4c022c50d7c1d7043b6b96776
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-26 10:43:42 -0300

    feat: Criado a interface AdminRepository para a classe Admin ter acesso ao banco de dados

--- src/main/java/br/com/zup/security/repositories/AdminRepository.java
+++ src/main/java/br/com/zup/security/repositories/AdminRepository.java
@@ -1,4 +1,10 @@
 package br.com.zup.security.repositories;
 
-public class AdminRepository {
+import br.com.zup.security.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<User, String> {
+    Optional<User> findByUserName(String userName);
 }
