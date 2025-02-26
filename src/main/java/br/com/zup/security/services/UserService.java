192595110b79548458075f7dbb0845a5a5aec9c6
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-26 10:54:03 -0300

    feat: Criado a classe UserService para realizar as atividades a logica de negocios da entidade user

--- src/main/java/br/com/zup/security/services/UserService.java
+++ src/main/java/br/com/zup/security/services/UserService.java
@@ -3,19 +3,18 @@ package br.com.zup.security.services;
 import br.com.zup.security.models.User;
 import br.com.zup.security.repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+@Service
 public class UserService {
     private UserRepository userRepository;
-    private PasswordEncoder bCryptPasswordEnconcer;
 
-    public UserService(UserRepository userRepository, PasswordEncoder bCryptPasswordEnconcer) {
+    public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
-        this.bCryptPasswordEnconcer = bCryptPasswordEnconcer;
     }
 
-    public User saveUser(User user){
-        String passwordEnconder = bCryptPasswordEnconcer.encode(user.getPassword());
-        user.setPassword(passwordEnconder);
-        return userRepository.save(user);
+    public Optional<User> findByUserName(String userName) {
+        return userRepository.findByUserName(userName);
     }
 }
