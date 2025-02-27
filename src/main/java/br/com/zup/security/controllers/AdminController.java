d8a7348a5b05a2a79a729afb484aa49915cfca31
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-27 09:09:43 -0300

    refactor: Criado endpoint Get com o metodo adminAcess para quando o admin logar na aplicacao

--- src/main/java/br/com/zup/security/controllers/AdminController.java
+++ src/main/java/br/com/zup/security/controllers/AdminController.java
@@ -1,4 +1,31 @@
 package br.com.zup.security.controllers;
 
+import br.com.zup.security.infra.JwtUtil;
+import br.com.zup.security.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
 public class AdminController {
+    private AdminService adminService;
+    private JwtUtil jwtUtil;
+
+    public ResponseEntity<?> getAdmin(@RequestHeader("Authorization") String token) {
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+
+            String role = jwtUtil.extractClaim(token, claims -> claims.get("role", String.class));
+
+            if ("ROLE_ADMIN".equals(role)) {
+                return ResponseEntity.ok().body("Bem vindo ao painel de administração");
+            }
+        }
+        return ResponseEntity.status(403).body("Acesso proibido. Você não tem permissão");
+    }
+
+    @GetMapping("/admin")
+    public String adminAcess(){
+        return "Conteúdo de administrador";
+    }
 }
---
+++ src/main/java/br/com/zup/security/dto/UserLoginDTO.java
@@ -0,0 +1,4 @@
+package br.com.zup.security.dto;
+
+public class UserLoginDTO {
+}
