2d171ca5b87688668c7270a711cec96922c650e4
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-27 11:32:24 -0300

    refactor: Alterado metodo na interface para buscar uma lista ao inves de um optional

--- src/main/java/br/com/zup/security/repositories/UserRepository.java
+++ src/main/java/br/com/zup/security/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package br.com.zup.security.repositories;
 import br.com.zup.security.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByUserName(String username);
+    List<User> findByUserName(String username);
 }
