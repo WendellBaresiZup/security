061849107f178d414fd426bc18caed30b72c3685
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-27 09:11:53 -0300

    feat: Adiciona UserLoginDTO classe que vai cuidar do login do usuario

--- src/main/java/br/com/zup/security/dto/UserLoginDTO.java
+++ src/main/java/br/com/zup/security/dto/UserLoginDTO.java
@@ -1,4 +1,25 @@
 package br.com.zup.security.dto;
 
 public class UserLoginDTO {
+    private String userName;
+    private String password;
+
+    public UserLoginDTO() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
