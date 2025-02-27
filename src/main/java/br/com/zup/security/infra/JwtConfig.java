2be996b6bb88cd33ecd44bfffd75def30de05c51
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-27 11:25:25 -0300

    feat: Criado classe jwtConfig para cuidar da criacao da chave secreta

--- src/main/java/br/com/zup/security/infra/JwtConfig.java
+++ src/main/java/br/com/zup/security/infra/JwtConfig.java
@@ -1,4 +1,17 @@
 package br.com.zup.security.infra;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.Key;
+@Configuration
 public class JwtConfig {
+    private static final String SECRET_KEY = "secreta";
+
+    @Bean
+    public Key key(){
+        return Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
 }
