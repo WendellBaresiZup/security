c7b75bc64452ce5e01f1204e51e593c328aaff76
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-27 09:12:52 -0300

    refactor: Alterado metodo de salvar usuario. Problema estava recebendo o user entidade ao inves do userDTO

--- src/main/java/br/com/zup/security/services/UserService.java
+++ src/main/java/br/com/zup/security/services/UserService.java
@@ -1,11 +1,14 @@
 package br.com.zup.security.services;
 
+import br.com.zup.security.dto.UserDTO;
+import br.com.zup.security.dto.UserLoginDTO;
 import br.com.zup.security.models.User;
 import br.com.zup.security.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Optional;
 @Service
 public class UserService {
@@ -21,9 +24,27 @@ public class UserService {
         return userRepository.findByUserName(userName);
     }
 
-    public User saveUser(User user){
+    public User saveUser(UserDTO userDTO){
+        User user = new User();
+        user.setUserName(userDTO.getUserName());
+        user.setPassword(userDTO.getPassword());
+
         String passwordEncoded = bCryptPasswordEncoder.encode(user.getPassword());
         user.setPassword(passwordEncoded);
         return userRepository.save(user);
+    }
+
+    public Map<String, String> login(UserLoginDTO userLoginDTO) {
+        Optional<User> userOptional = userRepository.findByUserName(userLoginDTO.getUserName());
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+
+            if (bCryptPasswordEncoder.matches(userLoginDTO.getPassword(), user.getPassword())) {
+                return Map.of("message", "usuário logado");
+            }
+            return Map.of("message","usuário inválido");
+        }
+        return Map.of("message", "userName inválido");
     }
 }
